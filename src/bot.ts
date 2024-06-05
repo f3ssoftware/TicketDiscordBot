92dce9093dc690b8abfc132f1cb00d28cabfeca2
Mateus Cavalcanti <mateusmartinscg@gmail.com>
2024-06-05 16:27:37 -0300

    resolving interection failed

--- src/bot.ts
+++ src/bot.ts
@@ -1,4 +1,4 @@
-import { Client, GatewayIntentBits } from "discord.js";
+import { Client, GatewayIntentBits, Interaction, InteractionType } from 'discord.js';
 import config from "./config";
 import * as commandModules from "./commands"
 import axios from "axios";
@@ -6,36 +6,42 @@ import axios from "axios";
 const commands = Object(commandModules)
 
 export const client = new Client({
-    intents: [
-      GatewayIntentBits.Guilds,
-      GatewayIntentBits.GuildMessages,
-      GatewayIntentBits.DirectMessages
-    ]
-  });
-
-  client.once("ready", () =>{
-    console.log("Discord bot ready!")
-  });
-
-  client.on('interactionCreate', async interaction => {
-    if (interaction.isCommand()) {
-        const { commandName } = interaction;
-        commands[commandName].execute(interaction, client);
-    } else if (interaction.isButton()) {
-        if (interaction.customId === 'resolve_ticket') {
-            const { channelId } = interaction;
-
-            try {
-                await axios.post('https://ticketdiscordbot.onrender.com/resolve', { threadId: channelId });
-            } catch (error) {
-                console.log(channelId);
-                await interaction.reply({
-                    content: 'An error occurred while updating the ticket status.',
-                    ephemeral: true,
-                });
-            }
-        }
-    }
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.DirectMessages
+  ]
 });
 
-  client.login(config.DISCORD_TOKEN)
+client.once("ready", () => {
+  console.log("Discord bot ready!")
+});
+
+client.on('interactionCreate', async (interaction: Interaction) => {
+  if (interaction.type === InteractionType.ApplicationCommand) {
+    const { commandName } = interaction;
+    if (commands[commandName]) {
+      commands[commandName].execute(interaction, client);
+    }
+  } else if (interaction.type === InteractionType.MessageComponent) {
+    if (interaction.customId === 'resolve_ticket') {
+      const { channelId } = interaction;
+
+      try {
+        await axios.post('https://ticketdiscordbot.onrender.com/resolve', { threadId: channelId });
+        await interaction.reply({
+          content: 'The ticket has been marked as resolved and the thread is now archived.',
+          ephemeral: true,
+        });
+      } catch (error) {
+        console.log(error);
+        await interaction.reply({
+          content: 'An error occurred while updating the ticket status.',
+          ephemeral: true,
+        });
+      }
+    }
+  }
+});
+
+client.login(config.DISCORD_TOKEN)
