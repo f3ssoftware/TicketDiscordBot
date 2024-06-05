4c136f0743ca9f9fb8a1bdb7e001d28200e5dace
Mateus Cavalcanti <mateusmartinscg@gmail.com>
2024-06-04 23:15:17 -0300

    initializer change

--- src/bot.ts
+++ src/bot.ts
@@ -25,24 +25,25 @@ export const client = new Client({
     commands[commandName].execute(interaction, client)
   })
 
-  client.on('interactionCreateBtn', async interaction => {
-    if (!interaction.isButton()){
-      return;
-    }
+  client.on('interactionCreate', async interaction => {
+    if (interaction.isCommand()) {
+        const { commandName } = interaction;
+        commands[commandName].execute(interaction, client);
+    } else if (interaction.isButton()) {
+        if (interaction.customId === 'resolve_ticket') {
+            const { channelId } = interaction;
 
-    if (interaction.customId === 'resolve_ticket') {
-      const { channelId } = interaction;
-
-        try {
-            await axios.post('https://ticketdiscordbot.onrender.com/resolve', { threadId: channelId });
-        } catch (error) {
-          console.log(channelId)
-            await interaction.reply({
-                content: 'An error occurred while updating the ticket status.',
-                ephemeral: true,
-            });
+            try {
+                await axios.post('https://ticketdiscordbot.onrender.com/resolve', { threadId: channelId });
+            } catch (error) {
+                console.log(channelId);
+                await interaction.reply({
+                    content: 'An error occurred while updating the ticket status.',
+                    ephemeral: true,
+                });
+            }
         }
     }
-})
+});
 
   client.login(config.DISCORD_TOKEN)
