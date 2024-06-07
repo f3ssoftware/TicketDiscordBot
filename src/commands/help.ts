52cba4ebb1028dc3a82cf209127e28427152efeb
Mateus Cavalcanti <mateusmartinscg@gmail.com>
2024-06-06 23:43:00 -0300

    new json translate

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -5,25 +5,32 @@ import {
     ButtonStyle,
 } from "discord.js";
 import { createTicket } from "../firebase";
-import {translations} from "../bot"
+import { translations } from "../bot";
 
-export const data = new SlashCommandBuilder().setName("help").setDescription("Creates a new help ticket.").addStringOption(option => option.setName("description").setDescription("Describe your problem").setRequired(true));
+export const data = new SlashCommandBuilder()
+    .setName("help")
+    .setDescription("Creates a new help ticket.")
+    .addStringOption(option => 
+        option.setName("description")
+            .setDescription(translations['en']['problem_description']) // Use the default language for the description
+            .setRequired(true)
+    );
 
 export async function execute(interaction: CommandInteraction, client: Client, selectedLanguage: string) {
     if (!interaction?.channelId) {
-        return
+        return;
     }
 
-    const channel = await client.channels.fetch(interaction.channelId)
+    const channel = await client.channels.fetch(interaction.channelId);
     if (!channel || channel.type != ChannelType.GuildText) {
-        return
+        return;
     }
 
     const thread = await (channel as TextChannel).threads.create({
-        name: `support-${Date.now()}`,
-        reason: `Support ticket ${Date.now()}`,
+        name: `${translations[selectedLanguage]['support_ticket']}-${Date.now()}`,
+        reason: `${translations[selectedLanguage]['support_ticket']} ${Date.now()}`,
         type: 12
-    })
+    });
 
     const problemDescription = (interaction.options.get('description')?.value as string) || '';
     const { user } = interaction;
@@ -39,16 +46,17 @@ export async function execute(interaction: CommandInteraction, client: Client, s
     );
 
     thread.send({
-        content: `**User:** ${user}\n**Problem:** ${problemDescription}`,
+        content: `**${translations[selectedLanguage]['user']}:** ${user}\n**${translations[selectedLanguage]['problem']}:** ${problemDescription}`,
         components: [resolveButton]
     });
 
-    //create the ticket and store it in the firestore
-    await createTicket(thread.id, problemDescription)
+    // Create the ticket and store it in Firestore
+    await createTicket(thread.id, problemDescription);
 
     return interaction.reply({
         content: translations[selectedLanguage]['help_message'],
         ephemeral: true,
     });
 }
+
 
