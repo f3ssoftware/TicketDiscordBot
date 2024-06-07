79ccd28b448f5e2148109664fd4d3728c765eaf3
Mateus Cavalcanti <mateusmartinscg@gmail.com>
2024-06-06 23:16:25 -0300

    language translate almost done

--- src/buttonHelper.ts
+++ src/buttonHelper.ts
@@ -3,15 +3,15 @@ import { ActionRowBuilder, ButtonBuilder, ButtonStyle } from 'discord.js';
 export function createLanguageSelectionButtons() {
     return new ActionRowBuilder<ButtonBuilder>().addComponents(
         new ButtonBuilder()
-            .setCustomId('select_language_en')
+            .setCustomId('select_english')
             .setLabel('English')
             .setStyle(ButtonStyle.Primary),
         new ButtonBuilder()
-            .setCustomId('select_language_es')
+            .setCustomId('select_spanish')
             .setLabel('Español')
             .setStyle(ButtonStyle.Primary),
             new ButtonBuilder()
-            .setCustomId('select_language_pt')
+            .setCustomId('select_portuguese')
             .setLabel('Português')
             .setStyle(ButtonStyle.Primary),
 
